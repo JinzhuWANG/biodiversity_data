5adc32d050d4d0a7f5a63c29a46dbe70f8de8205
Jinzhu WANG <wangjinzhulala@gmail.com>
2024-05-20 17:07:33 +1000

    Parallel the TIF2NC process

--- Save_tif_to_nc.py
+++ Save_tif_to_nc.py
@@ -1,40 +1,43 @@
-from codes.helper_func import replace_with_nearest
+import numpy as np
 import rioxarray as rxr
 import xarray as xr
 import pandas as pd
 
-
-
+from codes.helper_func import replace_with_nearest
+from tqdm.auto import tqdm
+from joblib import Parallel, delayed
 
 # Load the csv file
 df = pd.read_csv('data/all_suitability_tifs.csv')
+df = df.query('ssp != "historic"')
+df['path'] = df['path'].str.replace('N:\\Data-Master\\', 'N:\\Planet-A\\Data-Master\\')
+
+out_base = 'N:/Planet-A/LUF-Modelling/LUTO2_JZ/biodiversity_data/data'
 
 
-# Loop through each model
-for idx, group in df.groupby('model'):break
 
-xr_group = []
-for _, row in group.iterrows():
+
+def process_row(row):
     ds = rxr.open_rasterio(row['path'])
     ds.values = replace_with_nearest(ds.values, ds.rio.nodata)
-    ds.attrs = {}
-    
     dims = {k: [v] for k, v in dict(row).items() if k != 'path'}
-    ds = ds.expand_dims(dims)
-    xr_group.append(ds)
+    return ds.expand_dims(dims)
 
+# Loop through each model
+for idx, group in df.groupby('model'):
 
-xr_group = xr.combine_by_coords(xr_group, combine_attrs='override')
-xr_group.name = 'data'
-encoding = {'data': {"compression": "gzip", "compression_opts": 9}}
-xr_group.to_netcdf(f'out_raster/{idx}.nc', encoding=encoding, engine='h5netcdf')
+    pbar = tqdm(total=len(group))
+    para_obj = Parallel(n_jobs=-1, prefer="threads", return_as='generator')
+    tasks = (delayed(process_row)(row) for _, row in group.iterrows())
+    
+    xr_group = []
+    for result  in para_obj(tasks):
+        xr_group.append(result)
+        pbar.update(1)
 
-
-f = xr.open_dataset(f'out_raster/{idx}.nc')
-
-
-
-
-
-
-
+    xr_group = xr.combine_by_coords(xr_group, fill_value=0)
+    xr_group.name = 'data'
+    encoding = {'data': {"compression": "gzip", "compression_opts": 9}}
+    xr_group.to_netcdf(f'{out_base}/bio_{idx}.nc', encoding=encoding, engine='h5netcdf')
+    
+    pbar.close()
