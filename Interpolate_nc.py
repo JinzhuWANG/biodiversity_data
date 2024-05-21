1087b759da664f4a9e11ba34580ff0363aca5584
Jinzhu WANG <wangjinzhulala@gmail.com>
2024-05-21 14:59:58 +1000

    Save tif to nc

---
+++ Interpolate_nc.py
@@ -0,0 +1,35 @@
+import pickle
+import xarray as xr
+import rioxarray as rxr
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Save_tif_to_nc.py
+++ Save_tif_to_nc.py
@@ -1,8 +1,10 @@
+import os
 import numpy as np
 import rioxarray as rxr
 import xarray as xr
 import pandas as pd
 
+from glob import glob
 from codes.helper_func import replace_with_nearest
 from tqdm.auto import tqdm
 from joblib import Parallel, delayed
@@ -12,32 +14,40 @@ df = pd.read_csv('data/all_suitability_tifs.csv')
 df = df.query('ssp != "historic"')
 df['path'] = df['path'].str.replace('N:\\Data-Master\\', 'N:\\Planet-A\\Data-Master\\')
 
+
 out_base = 'N:/Planet-A/LUF-Modelling/LUTO2_JZ/biodiversity_data/data'
+exist_models = [os.path.basename(i)[4:-3] for i in glob(f'{out_base}/*.nc')]
+exist_models += ['GCM-Ensembles']
+
+df = df.query('model not in @exist_models')
 
 
 
 
-def process_row(row):
+def process_row(row, dims=None):
     ds = rxr.open_rasterio(row['path'])
     ds.values = replace_with_nearest(ds.values, ds.rio.nodata)
-    dims = {k: [v] for k, v in dict(row).items() if k != 'path'}
+    dims = {k: [v] for k, v in dict(row).items() if k != 'path'} if dims is None else dims
     return ds.expand_dims(dims)
+
+
 
 # Loop through each model
 for idx, group in df.groupby('model'):
-
-    pbar = tqdm(total=len(group))
+    
+    out_path = f'{out_base}/bio_{idx}.nc'
+    encoding = {'data': {"compression": "gzip", "compression_opts": 9}}
     para_obj = Parallel(n_jobs=-1, prefer="threads", return_as='generator')
     tasks = (delayed(process_row)(row) for _, row in group.iterrows())
+    pbar = tqdm(total=len(group))
     
-    xr_group = []
+    xr_chunk = []
     for result  in para_obj(tasks):
-        xr_group.append(result)
+        xr_chunk.append(result)
         pbar.update(1)
 
-    xr_group = xr.combine_by_coords(xr_group, fill_value=0)
-    xr_group.name = 'data'
-    encoding = {'data': {"compression": "gzip", "compression_opts": 9}}
-    xr_group.to_netcdf(f'{out_base}/bio_{idx}.nc', encoding=encoding, engine='h5netcdf')
-    
+    xr_chunk = xr.combine_by_coords(xr_chunk, fill_value=0)
+    xr_chunk.name = 'data'
+    xr_chunk.to_netcdf(out_path, mode='a', encoding=encoding, engine='h5netcdf')
+        
     pbar.close()
