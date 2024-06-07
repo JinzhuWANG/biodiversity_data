e23525bd0b6027801fba4ad9ef014ae827d18ab5
jinzhu <wangjinzhulala@gmail.com>
2024-06-07 14:35:33 +1000

    Use luto coord_lon_lat to get biodata

--- Interpolate_nc.py
+++ Interpolate_nc.py
@@ -10,38 +10,28 @@ ds = xr.open_dataset('data/ssp245_EnviroSuit.nc', engine='h5netcdf', chunks='aut
 
 
 # Load index
-index_dense = np.load("data/mask_idx_2d_dense.npy")
-index_sparse = np.load("data/mask_idx_2d_sparse.npy")
+index_dense = np.load("data/coord_lon_lat.npy")
 
-NLUMASK = rxr.open_rasterio(f"data/NLUM_2010-11_mask.tif")
-trans = NLUMASK.rio.transform()
-
-def get_coord(index_ij):
-    coord_x = trans.c + trans.a * index_ij[1]
-    coord_y = trans.f + trans.e * index_ij[0]
-    return xr.DataArray(coord_x, dims='points'), xr.DataArray(coord_y, dims='points')
-
-coord_dense = get_coord(index_dense)
-coord_sparse = get_coord(index_sparse)
-
+coord_x = xr.DataArray(index_dense[0], dims='points')
+coord_y = xr.DataArray(index_dense[1], dims='points')
 
 
 
 # Get the nc file with adjusted chunk sizes
-select = {
-    'species': ['Arenophryne_rotunda'],
-}
+ds_species = ds.sel(species=['Abelmoschus_ficulneus'])
+
+ds_interp = ds_species.interp(
+    year=[2010], 
+    x=np.linspace(ds.x.min(), ds.x.max(), len(ds.x) * 5),
+    y=np.linspace(ds.y.min(), ds.y.max(), len(ds.y) * 5),
+    method='linear', 
+    kwargs={"fill_value": "extrapolate"},
+)
+
+ds_sel = ds_interp.sel(
+    x=coord_x, 
+    y=coord_y, 
+    method='nearest')
 
 
-f_nc_sel = ds.sel(**select)
-f_nc_year = f_nc_sel.interp(year=[2010], 
-                            method='linear', 
-                            kwargs={"fill_value": "extrapolate"},
-                            x=np.linspace(ds.x.min(), ds.x.max(), len(ds.x) * 5),
-                            y=np.linspace(ds.y.min(), ds.y.max(), len(ds.y) * 5)
-            )
-
-f_nc_coords = f_nc_year.sel(x=coord_sparse[0], y=coord_sparse[1], method='nearest')
-
-
-plt.scatter(f_nc_coords.x, f_nc_coords.y, c=f_nc_coords.values.flatten(), cmap='viridis', s=0.01)
+plt.scatter(coord_x, coord_y, c=ds_sel.values.flatten(), cmap='viridis', s=0.01)
