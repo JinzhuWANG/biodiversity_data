1dd7c310b3100ba39eab8f9f9088c961528162d0
jinzhu <wangjinzhulala@gmail.com>
2024-06-11 15:53:07 +1000

    Get interpolated values for each year between 2010-2100

--- Interpolate_nc.py
+++ Interpolate_nc.py
@@ -6,14 +6,19 @@ from joblib import Parallel, delayed
 from dask.diagnostics import ProgressBar
 
 
+# Define parameters
+year_start = 2010
+year_end = 2100
+species_chunk_size = 1000
+
+
 # Load the data
 ds_enssemble = xr.open_dataset('data/ssp245_EnviroSuit.nc', engine='h5netcdf', chunks='auto')['data']
 ds_historic = xr.open_dataset('data/historic_historic.nc', engine='h5netcdf', chunks='auto')['data']
 
 # Split the ds into chunks at the species dimension
-chunk_size = 1000
 species_num = len(ds_enssemble['species'])
-species_chunks = np.array_split(np.arange(species_num), chunk_size)
+species_chunks = np.array_split(np.arange(species_num), species_chunk_size)
 
 # Fix the rounding error for x/y coords between the two datasets
 ds_historic['y'] = ds_enssemble['y']
@@ -27,10 +32,10 @@ coord_x = xr.DataArray(coord_lon_lat[0], dims='points')
 coord_y = xr.DataArray(coord_lon_lat[1], dims='points')
 
 
-def interp_sel(ds, up_scale=5, coord_x=coord_x, coord_y=coord_y):
+def interp_sel(ds, year, up_scale=5, coord_x=coord_x, coord_y=coord_y):
     # Interpolate, filter and round
     ds_interp = ds.interp(
-        year=[2030], 
+        year=[year], 
         x=np.linspace(ds.x.min(), ds.x.max(), len(ds.x) * up_scale),
         y=np.linspace(ds.y.min(), ds.y.max(), len(ds.y) * up_scale),
         method='linear', 
@@ -50,13 +55,25 @@ def interp_sel(ds, up_scale=5, coord_x=coord_x, coord_y=coord_y):
     return val
 
 
-# Create tasks 
-tasks = [delayed(interp_sel)(ds.isel(species=species_chunk)) for species_chunk in species_chunks]
-para_obj = Parallel(n_jobs=30, return_as='generator')
+# Get the interpolated values for each year
+for year in range(year_start, year_end + 1):
+    
+    print(f"Processing year {year}")
 
-# Parallel processing to get the interpolated values
-val_pts = []
-for out in tqdm(para_obj(tasks), total=len(tasks)):
-    val_pts.append(out)
+    tasks = [delayed(interp_sel)(ds.isel(species=species_chunk), year) 
+            for species_chunk in species_chunks]
 
+    para_obj = Parallel(n_jobs=30, return_as='generator')
+
+    # Parallel processing to get the interpolated values
+    val_pts = []
+    for out in tqdm(para_obj(tasks), total=len(tasks)):
+        val_pts.append(out)
+
+# Combine the results
 val_pts = xr.combine_by_coords(val_pts)
+val_pts.name = 'data'
+
+# Save to nc
+encoding = {'data': {"compression": "gzip", "compression_opts": 9,  "dtype": 'int8'}} 
+val_pts.to_netcdf(f'data/bio_ensemble_interpolated.nc', mode='w', encoding=encoding, engine='h5netcdf')
