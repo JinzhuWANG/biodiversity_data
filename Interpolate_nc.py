f19c27b39337930e8cb6f81d1274165f8b906511
jinzhu <wangjinzhulala@gmail.com>
2024-06-11 12:14:28 +1000

    Convert historic biodiversity data to nc

--- Interpolate_nc.py
+++ Interpolate_nc.py
@@ -1,32 +1,33 @@
 import xarray as xr
-import rioxarray as rxr
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+
+from dask.diagnostics import ProgressBar
 
 
 # Load the data
-ds = xr.open_dataset('data/ssp245_EnviroSuit.nc', engine='h5netcdf', chunks='auto')['data']
+ds_enssemble = xr.open_dataset('data/ssp245_EnviroSuit.nc', engine='h5netcdf', chunks='auto')['data']
+ds_historic = xr.open_dataset('data/historic_historic.nc', engine='h5netcdf', chunks='auto')['data']
 
+# Fix the rounding error for x/y coords between the two datasets
+ds_historic['y'] = ds_enssemble['y']
+ds_historic['x'] = ds_enssemble['x']
+ds = xr.combine_by_coords([ds_historic, ds_enssemble])['data']
 
 # Load index
-index_dense = np.load("data/coord_lon_lat.npy")
-
-coord_x = xr.DataArray(index_dense[0], dims='points')
-coord_y = xr.DataArray(index_dense[1], dims='points')
-
+coord_lon_lat = np.load("data/coord_lon_lat_res1.npy")
+coord_x = xr.DataArray(coord_lon_lat[0], dims='points')
+coord_y = xr.DataArray(coord_lon_lat[1], dims='points')
 
 
-# Get the nc file with adjusted chunk sizes
-ds_species = ds.sel(species=['Abelmoschus_ficulneus'])
-
-ds_interp = ds_species.interp(
-    year=[2010], 
+# Interpolate, filter and round
+ds_interp = ds.interp(
+    year=[2030], 
     x=np.linspace(ds.x.min(), ds.x.max(), len(ds.x) * 5),
     y=np.linspace(ds.y.min(), ds.y.max(), len(ds.y) * 5),
     method='linear', 
     kwargs={"fill_value": "extrapolate"},
-)
+).round().astype(np.int8)
 
 ds_sel = ds_interp.sel(
     x=coord_x, 
@@ -34,4 +35,8 @@ ds_sel = ds_interp.sel(
     method='nearest')
 
 
-plt.scatter(coord_x, coord_y, c=ds_sel.values.flatten(), cmap='viridis', s=0.01)
+# Get the values
+with ProgressBar():
+    val = ds_sel.data.compute()
+
+# plt.scatter(coord_x, coord_y, c=ds_sel.values.flatten(), cmap='viridis', s=0.01)
--- Save_tif_to_nc.py
+++ Save_tif_to_nc.py
@@ -12,15 +12,19 @@ from glob import glob
 from joblib import Parallel, delayed
 
 
-# # Search for all tif files and save to csv
+# Search for all tif files and save to csv
 # bio_path = r'N:\Data-Master\Biodiversity\Environmental-suitability\Annual-species-suitability_20-year_snapshots_5km'
 # get_all_path(bio_path)
 
 # Load the csv file
 df = pd.read_csv('data/all_suitability_tifs.csv')
-df = df.query('ssp != "historic"')
 
 ensemble_df = df.query('model == "GCM-Ensembles"').drop(columns=['group','model'])
+valid_species = ensemble_df['species'].unique()                  
+
+# Get the historical data. NOTE: Some species only have historical data, so we need to filter them out
+historic_df = df.query('model == "historic" and species.isin(@valid_species)')
+
 
 
 def process_row(row):
@@ -29,17 +33,14 @@ def process_row(row):
     return ds.expand_dims({'year':[row['year']], 'species':[row['species']]})
 
 
-
-for ssp, mode in product(ensemble_df['ssp'].unique(), ensemble_df['mode'].unique()):
-    
+def save_to_nc(df, ssp, mode):
+    # Check if the file already exists
     if os.path.exists(f'data/{ssp}_{mode}.nc'):
         print(f'{ssp}_{mode}.nc already exists')
-        continue
-    
-    # Filter the df
-    in_df = ensemble_df.query(f'ssp == "{ssp}" and mode == "{mode}"')
- 
+        return
+
     # Multi-threading to read TIF and expand dims
+    in_df = df.query(f'ssp == "{ssp}" and mode == "{mode}"')
     para_obj = Parallel(n_jobs=-1, return_as='generator')
     tasks = (delayed(process_row)(row) for _, row in in_df.iterrows())
     pbar = tqdm(total=len(in_df))
@@ -54,12 +55,16 @@ for ssp, mode in product(ensemble_df['ssp'].unique(), ensemble_df['mode'].unique
     # Combine the data and write to nc
     xr_chunk = xr.combine_by_coords(xr_chunk, fill_value=0, combine_attrs='drop')
     xr_chunk.name = 'data'
- 
+
     # Save to nc   
     encoding = {'data': {"compression": "gzip", "compression_opts": 9,  "dtype": 'int8'}} 
     xr_chunk.to_netcdf(f'data/{ssp}_{mode}.nc', mode='w', encoding=encoding, engine='h5netcdf')
-            
+                
 
-
-            
-            
+        
+# Save ensemble data to nc            
+for ssp, mode in product(ensemble_df['ssp'].unique(), ensemble_df['mode'].unique()):
+    save_to_nc(ensemble_df, ssp, mode)
+    
+# Save historic data to nc
+save_to_nc(historic_df, 'historic', 'historic')
